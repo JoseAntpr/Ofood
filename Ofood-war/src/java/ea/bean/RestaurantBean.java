b37e20cf86c23fd77e2822a8b1bb55ded802f6c6
UNI <UNI@UNI-PC>
2015-11-16 19:52:13 +0100

    parte funcionalidad de restaurant

--- Ofood-war/src/java/ea/bean/RestaurantBean.java
+++ Ofood-war/src/java/ea/bean/RestaurantBean.java
@@ -39,25 +39,10 @@ public class RestaurantBean {
     private LoginBean loginBean;
     
     @EJB
-    private UserFacade userFacade;
-    
-    @EJB
     private RestaurantFacade restaurantFacade;
     @EJB
-    private ItemCategoryFacade itemCategoryFacade;
-    @EJB
-    private ItemFacade itemFacade;
-    @EJB
-    private PurchaseOrderFacade purchaseOrderFacade;
-    
-    private User user;
-    
-    private Collection<ItemCategory> itemCategoryList;
-    private PurchaseOrder purchaseOrder;
-    private Restaurant restaurant;
+
     private float mark;
-  
-    
 
     /**
      * Creates a new instance of restaurantBean
@@ -73,30 +58,6 @@ public class RestaurantBean {
         this.loginBean = loginBean;
     }
 
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Collection<ItemCategory> getItemCategoryList() {
-        return itemCategoryList;
-    }
-
-    public void setItemCategoryList(Collection<ItemCategory> itemCategoryList) {
-        this.itemCategoryList = itemCategoryList;
-    }
-
-    public Restaurant getRestaurant() {
-        return restaurant;
-    }
-
-    public void setRestaurant(Restaurant restaurant) {
-        this.restaurant = restaurant;
-    }
-
     public float getMark() {
         return mark;
     }
@@ -104,52 +65,26 @@ public class RestaurantBean {
     public void setMark(float mark) {
         this.mark = mark;
     }
-
-    public PurchaseOrder getPurchaseOrder() {
-        return purchaseOrder;
-    }
-
-    public void setPurchaseOrder(PurchaseOrder purchaseOrder) {
-        this.purchaseOrder = purchaseOrder;
-    }
     
     @PostConstruct
     public void init(){
-            // Recuperar el usuario de la sesion
-            // Temporalmente
-            user = loginBean.getUser();
-//            user = userFacade.find(1);
-
-            // Ya tenemos el id del restaurante
-            Integer restaurantId = 2;
-            restaurant = restaurantFacade.find(restaurantId);
-
-            // Calculamos la media de la puntuación
-            mark = restaurantFacade.getRestaurantMark(restaurant);
-
-            if (restaurant != null){
-                itemCategoryList = itemCategoryFacade.findItemCategoryFromRestaurant(restaurantId);
-            }else{
-                System.out.println("Restaurante Nulo");
-            }
-            
-            purchaseOrder = loginBean.getPurchaseOrder();
-            
-        
+        mark = restaurantFacade.getRestaurantMark(loginBean.getRestaurant());
     }
     
-    @PreDestroy
-    public String addItem(Integer itemId){
-        if(itemId != null){
+    public void addItem(Item item){
+        if(item != null){
             PurchaseOrder po = loginBean.getPurchaseOrder();
-            if (po == null){
-                po = new PurchaseOrder(new Date(), "En proceso", 0.0f, loginBean.getUser().getAddress());
-            }
-            purchaseOrderFacade.addItem(itemId, po, user, restaurant);
-            loginBean.setPurchaseOrder(po);
-            loginBean.setItem(null);
+            po.getItemCollection().add(item);
+            po.setBill(po.getBill()+item.getPrice());
         }
-        return "restaurant";
+    }
+    
+    public void deleteItem(Item item){
+        if(item != null){
+            PurchaseOrder po = loginBean.getPurchaseOrder();
+            po.getItemCollection().remove(item);
+            po.setBill(po.getBill()-item.getPrice());
+        }
     }
     
 }
