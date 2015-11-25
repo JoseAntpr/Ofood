db4126ca2817e280b9c754ac10402a5998c36ec4
UNI <UNI@UNI-PC>
2015-11-25 13:01:37 +0100

    Búsqueda de restaurantes ordenada por puntuación FINALIZADO V2

--- Ofood-ejb/src/java/ea/ejb/RestaurantFacade.java
+++ Ofood-ejb/src/java/ea/ejb/RestaurantFacade.java
@@ -62,16 +62,12 @@ public class RestaurantFacade extends AbstractFacade<Restaurant> {
         return list;
     }
     
-    public List<String> getRestaurantPuntuationOrdered(){
-//        q = em.createQuery("SELECT r FROM Restaurant r ORDER BY (SELECT SUM(re.mark) FROM r.reviewCollection re), DESC");
-//        q = em.createQuery("SELECT r FROM Restaurant r ORDER BY SUM(r.reviewCollection.mark), DESC");
-        /*
-        UPDATE Publisher pub SET pub.status = 'outstanding'
-        WHERE pub.revenue < 1000000 AND 20 > (SELECT COUNT(mag) FROM pub.magazines mag)
-        */
-        Query q = em.createNativeQuery("select name from restaurant, review where "
-                + "restaurant_id = restaurant.id group by restaurant_id order by SUM(mark) DESC");
-        List<String> list = new LinkedList();
+    public List<Object[]> getRestaurantPuntuationOrdered(){
+    // "select name from restaurant, review where restaurant_id = restaurant.id group by restaurant_id order by SUM(mark) DESC        
+        Query q = em.createQuery( "SELECT r.name, (SUM(re.mark)*1.0)/COUNT(re) as mark FROM Restaurant r, Review re "
+                                + "WHERE r.id = re.restaurantId.id "
+                                + "GROUP BY r.id ORDER BY mark DESC");
+        List<Object[]> list = new LinkedList();
         try {
             list = new LinkedList(q.getResultList());
         } catch (Exception ex) {
